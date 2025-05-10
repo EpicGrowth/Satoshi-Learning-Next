bf73805d8e95ee7233adec0c1444d7177bea0ba0
Source Matrix <marketing@epicgrowth.com>
2025-05-10 12:14:51 +0200

    Fix layout component issues: add proper root layout and fix component imports

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import { Header } from './Header';
+import { Footer } from './Footer';
 
 interface LayoutProps {
   children: React.ReactNode;
